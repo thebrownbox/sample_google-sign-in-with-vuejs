2ae3dc992578f99950434c2f64c8ccc7c293425a
thebrownbox <conghv123@gmail.com>
2021-09-22 16:40:41 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
 import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
+import GAuth from 'vue3-google-oauth2'
+
+let gauthClientId = "770383407362-0e2coqiu3a22urvjfcon10r33atu4ile.apps.googleusercontent.com";
 
 
 createApp(App)
   .use(Quasar, quasarUserOptions)
-  .use(store)
-  .use(router)
+  .use(GAuth, { clientId: gauthClientId, scope: 'email', prompt: 'consent', fetch_basic_profile: true })
   .mount("#app");
